60ef4c1dc9369290a6803fba63076708e7b61906
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-04-12 09:26:55 -0400

    Update useTicker.ts

--- script/util/hook/useTicker.ts
+++ script/util/hook/useTicker.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Ticker, TickerName, getTicker } from '@/../script/state/service/ticker';
+import { Ticker, TickerName, getTicker } from '@/../script/state/repository/ticker';
 
 export default function useTicker (tickerName: TickerName, delay: number) {
   const [ticker, setTicker] = useState<Ticker>({ symbol: '', price: '' });
@@ -10,6 +10,7 @@ export default function useTicker (tickerName: TickerName, delay: number) {
   };
 
   useEffect(() => {
+    fetchTicker()
     const intervalId = setInterval(fetchTicker, delay);
     return () => clearInterval(intervalId);
   }, [tickerName]);
