c1b1768bc17e93d74170afc9cb977ce9a84355e9
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-04-12 09:25:19 -0400

    Update TickerCard.tsx

--- src/dom/atom/TickerCard.tsx
+++ src/dom/atom/TickerCard.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { Ticker, TickerName } from '@/../script/state/service/ticker';
+import { Ticker, TickerName } from '@/../script/state/repository/ticker';
 import useTicker from '@/../script/util/hook/useTicker';
 
 const Component = ({
-  tickerName = "BTCUSDT", initialTicker, delay = 10000
+  tickerName = "EURUSDT", initialTicker, delay = 10000
 }:{ tickerName: TickerName, initialTicker?: Ticker, delay?: number }) => {
+    
   const ticker = useTicker(tickerName, delay);
-  console.log("ticker",ticker)
+  
   return (<>
     {!!initialTicker && !ticker.price && 
       <p>
