37e5c639e19d5eb2d08d20d5114de2794069d691
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-04-12 07:56:47 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 export default async function Home() {
 
   const ticker = await (
-    await fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+    await fetch("https://api.binance.com/api/v3/ticker/price?symbol=EURUSDT")
   ).json()
   
   return (
     <main >
       <div >
-        <p> Bitcoin:&nbsp; <code >{ticker.price}</code> </p>
+        <p> Ticker Price:&nbsp; <code >{ticker.symbol}: {ticker.price}</code> </p>
         <div>
           <a href="/" target="_blank" rel="noopener noreferrer">
             By{' '}<Image src="/vercel.svg" alt="Vercel Logo" width={100} height={24} priority />
